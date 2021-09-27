3c04747067134a669af934f0d00c4f01d92632a0
Antonio <antoniogviegas@hotmail.com>
2021-09-28 01:18:20 +0200

    Add comments

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//When the app loads, inject the necessary content (this is the same in all apps)
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
         chrome.scripting.insertCSS({
@@ -19,17 +20,28 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     }
 });
 
+// When the foreground sends a message, receive it and...
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+    // If the foreground wants the token...
     if (request.message === 'get_token') {
+
+        // Get it with google drive API
         chrome.identity.getAuthToken({interactive: true}, function (token) {
             console.log("Token: ", token);
+
+            // Send the token back to the foreground
             sendResponse({
                 message: 'success',
                 payload: token
             });
         });
         return true;
+
+        // If the foreground wants to open the online viewer...
     } else if (request.message === 'open_ifc_viewer') {
+
+        // Open the viewer in a new tab, passing the id and the name as URL parameters
         const url = `http://localhost:5000/examples/web-ifc-viewer/google-drive-viewer/?id=${request.text}&name=${request.name}`
         chrome.tabs.create({ url });
         return true;
